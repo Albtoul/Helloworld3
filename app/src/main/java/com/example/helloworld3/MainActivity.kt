3f154f26d37233e30582ab66cad17c95c66f9f65
albtoul <albtoul-kaseem@hotmail.com>
2021-10-15 13:00:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld3/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.helloworld3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+val but= findViewById<Button>(R.id.button2)
+       but.setOnClickListener{
+           val intent = Intent(this,MainActivity2::class.java)
+           startActivity(intent)
+       }
+
+
+
+
+
+    }
+}
